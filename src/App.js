abf3eeb8062d1154b3b8d78600584c0a8c948084
allan <allanmathenge2@gmail.com>
2023-11-30 16:02:56 +0300

    commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Navbar from "./components/navbar/Navbar";
+import Home from "./components/home/Home";
+import Hero from "./components/hero/Hero";
+import Footer from "./components/footer/Footer";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="content">
+        <Navbar />
+        <Home />
+        <Hero />
+        <Footer />
+      </div>
     </div>
   );
 }
---
+++ src/components/navbar/Navbar.js
@@ -0,0 +1,65 @@
+import React from "react";
+import "./navbar.css";
+import logo from "../../assets/netlify-logo.svg";
+import { RiArrowDropDownLine } from "react-icons/ri";
+import { FaSearch } from "react-icons/fa";
+import { IoIosMenu } from "react-icons/io";
+
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      <div className="nav-links">
+        <div>
+          <a className="netlify-logo" href="/">
+            <img src={logo} alt="logo" />
+          </a>
+        </div>
+        <div className="link-list">
+          <li>
+            <a href="/">
+              Home <RiArrowDropDownLine />
+            </a>
+          </li>
+          <li>
+            <a href="/">
+              Platform <RiArrowDropDownLine />
+            </a>
+          </li>
+          <li>
+            <a href="/">Solutions</a>
+          </li>
+          <li>
+            <a href="/">
+              Integrations <RiArrowDropDownLine />
+            </a>
+          </li>
+          <li>
+            <a href="/">Start Building</a>
+          </li>
+          <li>
+            <a href="/">Docs</a>
+          </li>
+          <li>
+            <a href="/">Pricing</a>
+          </li>
+        </div>
+
+        <div className="auth">
+          <div className="search">
+            <FaSearch />
+          </div>
+          <div className="contact">Contact</div>
+          <div className="login">Log in</div>
+          <div>
+            <button className="signUpbutton">Sign Up</button>
+          </div>
+          <div className="menu">
+            <IoIosMenu />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
